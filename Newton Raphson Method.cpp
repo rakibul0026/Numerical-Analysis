1fcb98328b94f58c2c5c8dcc1ce92b1d3cc780e6
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-07-29 02:40:42 +0600

    Update Newton Raphson Method.cpp

--- Newton Raphson Method.cpp
+++ Newton Raphson Method.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+float fun(float x) {
+    return (x * sin(x) + cos(x));
+}
+float fun_Derivative(float x) {
+    return (sin(x) + x * cos(x));
+}
+int main() {
+    float x;
+    int i = 0, n;
+    cout << "Enter first approximation: ";
+    cin >> x; // Initial guess
+    cout << "Enter number of iterations: ";
+    cin >> n; // Number of iterations
 
+    while (i < n) {
+        float f_value = fun(x);
+        float derivative_value = fun_Derivative(x);
+
+        if (derivative_value == 0) {
+            cout << "Mathematical Error: Derivative is zero!" << endl;
+            break;
+        }
+
+        x = x - f_value / derivative_value;
+        i++;
+    }
+
+    cout << "Approximate root: " << x << endl;
+    return 0;
+}
