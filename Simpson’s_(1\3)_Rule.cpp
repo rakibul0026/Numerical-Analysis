7bd275da1de847fc3e3f4cdb082ae2e993a969b0
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2025-01-24 19:16:06 +0600

    Update Simpson’s_(1\3)_Rule.cpp

--- Simpson’s_(1\3)_Rule.cpp
+++ Simpson’s_(1\3)_Rule.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// #define f(x) log(x)  
+ #define f(x) 1/(1+x*x)
+
+int main()
+{
+    double lower, upper, interval;
+    cout << "Enter the lower and upper limit: " << endl;
+    cin >> lower >> upper;
+    cout << "Enter the value of interval" << endl;
+    cin >> interval;
+
+    double step = (upper - lower) / interval;
+    double ans = f(lower) + f(upper);
+    ans=ans;
+    
+    for (double i = 1; i < interval; i++)
+    {
+        double x = lower + i * step;
+        if((int)i%2!=0 )ans +=4*f(x);
+        else ans+= 2*f(x);
+    }
+    cout << ans *step/3 << endl;
+}
