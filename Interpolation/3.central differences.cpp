e04dcbcddb5eae3e74807efa2a47a858e6186562
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2025-01-29 03:42:04 +0600

    Update 3.central differences.cpp

--- Interpolation/3.central differences.cpp
+++ Interpolation/3.central differences.cpp
@@ -1,1 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x[20], y[20][20], i, j, n;
+    cout << "Enter the number of data points:" << endl;
+    cin >> n;
+
+    for (i = 0; i < n; i++)
+    {
+        cout << "x[" << i << "]: ";
+        cin >> x[i];
+        cout << "y[" << i << "]: ";
+        cin >> y[i][0];
+    }
+
+    for (i = 1; i < n; i++)
+    {
+        for (j = i; j < n - i; j++)
+        {
+            y[j][i] = y[j + 1][i - 1] - y[j - 1][i - 1];
+        }
+    }
+
+    cout << "Central Difference Table:" << endl;
+    for (i = 0; i < n; i++)
+    {
+        cout << "x[" << i << "] = " << x[i];
+        for (j = 0; j < n - i; j++)
+        {
+            cout << "\t" << y[i][j];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
 
