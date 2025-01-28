e57d895afcf310fe6b94d52b3634415d689f7e39
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2025-01-29 02:06:57 +0600

    Update Newton Raphson Method(EX-2.19).cpp

--- Newton Raphson Method/Newton Raphson Method(EX-2.19).cpp
+++ Newton Raphson Method/Newton Raphson Method(EX-2.19).cpp
@@ -1,1 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+double func(double x)
+{
+    return (4*exp(-x)*sin(x)-1);
+}
+double deri_func(double x)
+{
+    return 4*(exp(-x)*cos(x)-sin(x)*exp(-x));
+}
+
+int main()
+{
+    double x, n;
+    cout << "Enter the value of x:";
+    cin >> x;
+    cout << "Enter the value of iteration";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        double func_value = func(x);
+        double devi_value = deri_func(x);
+
+        if (devi_value == 0)
+        {
+            cout << "ERROR";
+            break;
+        }
+        x = x - func_value / devi_value;
+
+    }
+    cout<<"Root :"<<x<<endl;
+    return 0;
+
+}
