46ec706f5805db4cee94ddb36d256cc6ceda1fe3
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2025-01-29 19:28:31 +0600

    Update 4.Newton's_formula_for_interpolation(Example-3.4).cpp

--- Interpolation/4.Newton's_formula_for_interpolation(Example-3.4).cpp
+++ Interpolation/4.Newton's_formula_for_interpolation(Example-3.4).cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
     cout << "Enter the number of data points (n): ";
     cin >> n;
@@ -9,42 +10,41 @@ int main() {
     double x[100], y[100][100];
 
     cout << "Enter the values of x:\n";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> x[i];
     }
 
     cout << "Enter the values of y:\n";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> y[i][0];
     }
 
-    for (int i = 1; i < n; i++) {
-        for (int j = 0; j < n - i; j++) {
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
             y[j][i] = y[j + 1][i - 1] - y[j][i - 1];
         }
     }
     cout << "\nDivided Difference Table:\n";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cout << x[i];
-        for (int j = 0; j < n - i; j++) {
+        for (int j = 0; j < n - i; j++)
+        {
             cout << "\t" << y[i][j];
         }
         cout << endl;
     }
+    double h, b, a = 8, result;
+    h = x[1] - x[0];
+    b = (a - x[0]) / h;
 
-    double value = 8;  
-    double h = x[1] - x[0];  
-    double p = (value- x[0]) / h;
+    result = y[0][0] + b * y[0][1] + b * (b - 1) / 2 * y[0][2] + b * (b - 1) * (b - 2) / 6 * y[0][3];
 
-    double result = y[0][0];  
-    double term = 1; 
+    cout <<"Result is :"<<result;
 
-    for (int i = 1; i < n; i++) {
-        term *= (p - (i - 1)) / i;  
-        result += term * y[0][i];  
-    }
-
-    cout << "\nThe interpolated value at x = " << value << " is " << result << endl;
     return 0;
 }
-
